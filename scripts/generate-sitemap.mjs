cc47aedbdcb12c572a41549681c0fcf54ed946d0
joshuasparkes <joshsparkes6@gmail.com>
2024-07-29 10:57:32 +0100

    add sitemap

--- scripts/generate-sitemap.mjs
+++ scripts/generate-sitemap.mjs
@@ -3,9 +3,9 @@ import { globby } from "globby";
 
 async function generateSitemap() {
   const pages = await globby([
-    "src/pages/**/*.js", // Adjusted to the correct path and extension
-    "!src/pages/_*.js", // Exclude Next.js specific files like _app.js and _document.js
-    "!src/pages/api/**", // Exclude API routes
+    "src/app/**/*.js", // Adjusted to the correct path and extension
+    "!src/app/_*.js", // Exclude Next.js specific files like _app.js and _document.js
+    "!src/app/api/**", // Exclude API routes
   ]);
 
   console.log("pages", pages); // This will show which pages are being detected
@@ -15,8 +15,9 @@ async function generateSitemap() {
 ${pages
   .map((page) => {
     const path = page
-      .replace("src/pages", "") // Adjusted to the correct root path
+      .replace("src/app", "") // Adjusted to the correct root path
       .replace(".js", "")
+      .replace("/page", "") // Ensure '/page' at the end of a path is trimmed
       .replace("/index", ""); // Ensure '/index' at the end of a path is trimmed
     const route = path === "/index" ? "" : path; // Handle the root path correctly
 
