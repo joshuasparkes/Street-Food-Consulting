d24cef0164cb5369c871170634293e2d20d94966
joshuasparkes <joshsparkes6@gmail.com>
2024-07-29 13:45:59 +0100

    navbar tweak

--- src/app/NavBar.js
+++ src/app/NavBar.js
@@ -8,50 +8,52 @@ export default function NavBar() {
   const pathname = usePathname();
 
   return (
-    <nav className="w-full bg-black p-4 flex items-center justify-between h-20"> {/* Adjust height as needed */}
-        <Image src="/logo.jpeg" alt="Logo" width={50} height={50} />
-        <ul className="flex space-x-4">
-          <li>
-            <Link
-              href="/"
-              className={`text-white hover:text-gray-400 ${
-                pathname === "/" ? "border-b-2 border-white" : ""
-              }`}
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/contact"
-              className={`text-white hover:text-gray-400 ${
-                pathname === "/contact" ? "border-b-2 border-white" : ""
-              }`}
-            >
-              Contact
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/services"
-              className={`text-white hover:text-gray-400 ${
-                pathname === "/services" ? "border-b-2 border-white" : ""
-              }`}
-            >
-              Services
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/clients"
-              className={`text-white hover:text-gray-400 ${
-                pathname === "/clients" ? "border-b-2 border-white" : ""
-              }`}
-            >
-              Clients
-            </Link>
-          </li>
-        </ul>
+    <nav className="w-full bg-black p-4 flex items-center justify-between h-20">
+      {" "}
+      {/* Adjust height as needed */}
+      <Image src="/logo.jpeg" alt="Logo" width={50} height={50} />
+      <ul className="flex space-x-4">
+        <li>
+          <Link
+            href="/"
+            className={`text-white hover:font-semibold ${
+              pathname === "/" ? "border-b-2 border-white" : ""
+            }`}
+          >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/contact"
+            className={`text-white hover:font-semibold ${
+              pathname === "/contact" ? "border-b-2 border-white" : ""
+            }`}
+          >
+            Contact
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/services"
+            className={`text-white hover:font-semibold ${
+              pathname === "/services" ? "border-b-2 border-white" : ""
+            }`}
+          >
+            Services
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/clients"
+            className={`text-white hover:font-semibold ${
+              pathname === "/clients" ? "border-b-2 border-white" : ""
+            }`}
+          >
+            Clients
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 }
