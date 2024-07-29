7920fb24b73c3eef6ebefa140f2735b3e4399157
joshuasparkes <joshsparkes6@gmail.com>
2024-07-29 10:39:00 +0100

    add hj 2

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,20 @@
 import Image from "next/image";
 
 export default function Home() {
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.innerHTML = `(function(h,o,t,j,a,r){
+      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+      h._hjSettings={hjid:5075527,hjsv:6};
+      a=o.getElementsByTagName('head')[0];
+      r=o.createElement('script');r.async=1;
+      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+      a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`;
+    document.head.appendChild(script);
+  }, []);
+
   return (
     <div className="flex flex-col items-center">
       <Image src="/logo.jpeg" alt="Logo" width={300} height={300} />
